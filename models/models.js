490991e75b964a1c2165ed9814775d545434cf51
Jason Zuidijk <jason@Jasons-MBP-3.lan>
2025-04-29 14:43:53 +0100

    task 2, get api/topics

--- models/models.js
+++ models/models.js
@@ -1,2 +1,9 @@
-const db = require("../db/connection")
+const db = require("../db/connection");
 
+const selectTopics = () => {
+  return db.query("SELECT * FROM topics").then((result) => {
+    return result.rows;
+  });
+};
+
+module.exports = selectTopics;
